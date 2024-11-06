e240120594ee926a0dac54bb129f98c583ab9147
KillerBee-Web <sebastianmoncadaquinones@gmail.com>
2024-11-06 01:02:57 +0100

    Actualizacion para deslogeo

--- src/ProyectoWeb/base/urls.py
+++ src/ProyectoWeb/base/urls.py
@@ -4,8 +4,8 @@ from django.contrib.auth.views import LogoutView
 
 urlpatterns = [path('', ListaPendientes.as_view(), name = 'tareas'),
                path('login/', Logeo.as_view(), name = 'login'),
+               path('logout/', LogoutView.as_view(next_page='login'), name = 'logout'),
                path('registro/', PaginaRegistro.as_view(), name = 'registro'),
-               path('logout/', LogoutView.as_view(next_page= 'login'), name = 'logout'),
                path('tarea/<int:pk>', DetalleTarea.as_view(), name = 'tarea'),
                path('crear-tarea/', CrearTarea.as_view(), name = 'crear-tarea'),
                path('editar-tarea/<int:pk>', EditarTarea.as_view(), name = 'editar-tarea'),
